6e5909693ce24387a0a7f43e283e1bffbcf35d73
turdal1hasanboyev <turdalihasanboyev70.com>
2024-09-04 14:02:34 +0500

    Written moviefile model api!

--- apps/movie/admin.py
+++ apps/movie/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from .models import Movie, MovieImage, Review, MovieFile, AdditionalInfo, Liked
 
-admin.site.register(Movie)
 admin.site.register(MovieImage)
 admin.site.register(Review)
 admin.site.register(MovieFile)
 admin.site.register(AdditionalInfo)
 admin.site.register(Liked)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'created_at',
+    )
+
+    prepopulated_fields = {"slug": ["name"]}    
--- apps/news/admin.py
+++ apps/news/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import MovieNews, Tag
 
-admin.site.register(MovieNews)
 admin.site.register(Tag)
+
+
+@admin.register(MovieNews)
+class MovieNewsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'created_at',
+    )
+
+    prepopulated_fields = {"slug": ["name"]}
