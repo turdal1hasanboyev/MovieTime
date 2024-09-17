a3b04674e00aaa4b3450c8da7128b829307943f0
turdal1hasanboyev <turdalihasanboyev70.com>
2024-09-17 11:16:23 +0500

    Finish!

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,10 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-laq)^(vp)xjl^_q#3thikt%ee^v&kbgh6w*p_vblmr8507k6s@'
+SECRET_KEY = env.str("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
